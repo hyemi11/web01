d22365a14b0c67d8254895a12378bfc2250bcc98
hyemi1 <ham.c113@gmail.com>
2024-12-12 15:10:01 +0900

    복습업데이트

---
+++ 90.복습/4.naver_search복습/frontend/src/App.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import SearchBar from "./components/SearchBar";
+import SearchResult from "./components/SearchResult";
+
+const App = () => {
+  const [query, setQuery] = useState("");
+  const [result, setResult] = useState([]);
+  const [error, setError] = useState(null);
+
+  const handleSearch = async (query) => {
+    setQuery(query);
+    setError(null);
+
+    try {
+      const response = await fetch(
+        `/search/blog?query=${encodeURIComponent(query)}`
+      );
+      const data = await response.json();
+      if (data.items) {
+      } else {
+      }
+    } catch (err) {
+      setError(`요청실패: ${err}`);
+    }
+  };
+  return (
+    <div>
+      <h1>네이버 블로그 검색</h1>
+      <SearchBar onSearch={handleSearch} />
+      <SearchResult result={result} />
+    </div>
+  );
+};
+
+export default App;
---
+++ 90.복습/4.naver_search복습/frontend/src/components/SearchBar.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+const SearchBar = ({ onSearch }) => {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSearch(inputValue);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="검색어..."
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <button type="submit">검색</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchBar;
